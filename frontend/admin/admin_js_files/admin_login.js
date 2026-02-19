84023a1997a81c8fd4198b51608da45ead015056
sksahoo-techrayslabs <sksahoo@techrayslabs.com>
2026-02-19 16:16:36 +0530

    admin login page and admin login js added

---
+++ frontend/admin/admin_js_files/admin_login.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("adminloginform");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const admin = users.find(user =>
+        user.email === email &&
+        user.password === password &&
+        user.role === "admin"
+    );
+
+    if (!admin) {
+        alert("Invalid admin credentials!");
+        return;
+    }
+
+    localStorage.setItem("currentUser", JSON.stringify(admin));
+
+    window.location.href = "admin_product.html";
+});
